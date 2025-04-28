d0394c810125cd8f064a8e9d559b6336ad2dae29
esmanurunlu <esmanurunlu367@gmail.com>
2025-04-28 12:06:51 +0300

    Add files via upload

---
+++ 250428/ornek1.py
@@ -0,0 +1,4 @@
+sayi1=(input("sayi gir"))
+sayi2=(input("sayi gir"))
+for i in range (sayi1,sayi2):
+    print(i)
---
+++ 250428/ornek2.py
@@ -0,0 +1,6 @@
+toplam=0
+sayi1=int(input("sayi girin"))
+sayi2=int(input("sayi girin"))
+for sayi in range (sayi1,sayi2):
+    toplam=toplam+sayi
+print("toplam deger :",toplam)
